027bc78dbb43554d1cd548451086cc0cb2e663b0
ignjatovicpavle <pavle.ignjatovic.08+portfolio@gmail.com>
2025-05-07 19:35:27 +0200

    Physics practice questions setup
    Page for physics MCQ done. Automated test loading into single html page from javascript files. Linking to be explained to Thenura. Test for Newton's laws available as example

---
+++ Practice Questions/Physics/mcq/tests/newton.js
@@ -0,0 +1,40 @@
+export let title = "Newton's Laws MCQ Problems"
+
+export let problems = [
+    {
+        question:"Which of the following best describes Newton’s 1st Law?",
+        options:[
+            "\( F = ma \)", 
+            "An object remains in motion/rest unless acted on",
+            "Action = Reaction",
+            "Force is gravitational"
+        ],
+        correct:"B",
+        hint: "Think about inertia.",
+        detailed: "Newton’s First Law (law of inertia) says that an object at rest stays at rest, and an object in motion stays in motion, unless acted upon by a net external force."
+    },
+    {
+        question:"A 4 kg mass accelerates at \(3 \, \text{m/s}^2\). What is the net force?",
+        options:[
+            "6 N", 
+            "12 N",
+            "1.33 N",
+            "0.75 N"
+        ],
+        correct:"B",
+        hint: "Use \(F = ma\)",
+        detailed: "Newton’s Second Law: \( F = ma = 4 \times 3 = 12 \, \text{N} \)"
+    },
+    {
+        question:"Which law explains the recoil of a gun?",
+        options:[
+            "Newton’s 1st Law", 
+            "Newton’s 2nd Law",
+            "Newton’s 3rd Law",
+            "Hooke’s Law"
+        ],
+        correct:"C",
+        hint: "Which law talks about equal and opposite?",
+        detailed: "Newton’s Third Law explains that every action has an equal and opposite reaction. The recoil is the opposite reaction to the forward bullet motion."
+    }
+]
