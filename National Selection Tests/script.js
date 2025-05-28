c45a4bdcefadd5d7e5d63cd6f43aa17c38368713
Thenurawick <thenurawick@gmail.com>
2025-05-28 19:05:43 +0000

    removing countries from national selection Tests

--- National Selection Tests/script.js
+++ National Selection Tests/script.js
@@ -8,66 +8,12 @@ document.addEventListener("DOMContentLoaded", () => {
     },
     
     {
-      name: "Brazil",
-      difficulty: 1,
-      flag: "https://flagcdn.com/br.svg",
-      link: "Brazil/brazil.html"
-    },
-    {
-      name: "Costa Rica",
-      difficulty: 1,
-      flag: "https://flagcdn.com/cr.svg",
-      link: "Costa Rica/costarica.html"
-    },
-    {
-      name: "Cyprus",
-      difficulty: 1,
-      flag: "https://flagcdn.com/cy.svg",
-      link: "Cyprus/cyprus.html"
-    },
-    {
-      name: "Estonia",
-      difficulty: 1,
-      flag: "https://flagcdn.com/ee.svg",
-      link: "Estonia/estonia.html"
-    },
-    {
-      name: "Germany",
-      difficulty: 1,
-      flag: "https://flagcdn.com/de.svg",
-      link: "Germany/germany.html"
-    },
-    {
-      name: "Hong Kong",
-      difficulty: 1,
-      flag: "https://flagcdn.com/hk.svg",
-      link: "Hong Kong/hongkong.html"
-    },
-    {
-      name: "India",
-      difficulty: 1,
-      flag: "https://flagcdn.com/in.svg",
-      link: "India/india.html"
-    },
-    {
-      name: "Lithuania",
-      difficulty: 1,
-      flag: "https://flagcdn.com/lt.svg",
-      link: "Lithuania/lithuania.html"
-    },
-    {
       name: "Romania",
       difficulty: 1,
       flag: "https://flagcdn.com/ro.svg",
       link: "Romania/romania.html"
     },
     
-    {
-      name: "Serbia",
-      difficulty: 4,
-      flag: "https://flagcdn.com/rs.svg",
-      link: "Serbia/serbia.html"
-    },
     {
       //difficulty only integer for now
       name: "Sri Lanka",
