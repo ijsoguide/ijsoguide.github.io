a11c1511ec47dd289334bbbd3325febf222764d2
ignjatovicpavle <pavle.ignjatovic.08+portfolio@gmail.com>
2025-06-07 20:21:03 +0200

    Preload on team pages
    Hidden content until the page loads (excluding navigation bar and footer).

--- Our Team/preload.js
+++ Our Team/preload.js
@@ -1,5 +1,7 @@
+//document.getElementById('mainContent').style.display = "none";
 
 var loader = document.getElementById("preload")
 window.addEventListener("load", function(){
     loader.style.display = "none";
+    document.getElementById('mainContent').style.display = "block";
 })
