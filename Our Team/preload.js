438a9e2080f88a925f712f3593aedf2d58ed9cb8
ignjatovicpavle <pavle.ignjatovic.08+portfolio@gmail.com>
2025-06-20 10:38:37 +0200

    Preload on team page. Contact form fixed.

--- Our Team/preload.js
+++ Our Team/preload.js
@@ -1,7 +1,9 @@
-//document.getElementById('mainContent').style.display = "none";
+document.getElementById('mainContent').style.display = "none";
+document.querySelector('footer').style.display = "none";
 
 var loader = document.getElementById("preload")
 window.addEventListener("load", function(){
     loader.style.display = "none";
     document.getElementById('mainContent').style.display = "block";
+    document.querySelector('footer').style.display = "block";
 })
